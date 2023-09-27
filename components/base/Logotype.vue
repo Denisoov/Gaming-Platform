990c66f0e5928cdcf77d14be5cebff0fc6d39987
denisoov07 <denisoov07@gmail.com>
2023-09-27 23:30:29 +0700

    GAMING-3: Добавил логотип

---
+++ components/base/Logotype.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Assets
+import ALogoType from "@/assets/images/logotype.webp";
+</script>
+
+<template>
+  <div class="logotype">
+    <img
+      class="w-20 h-20 cursor-pointer object-contain"
+      :src="ALogoType"
+      alt="Логотип"
+    />
+  </div>
+</template>
